a0b6db89276d7c1a0fa9715ec5b2db5e05c989fa
Milagros Grimoldi <mgrimoldi@mail.austral.edu.ar>
2025-10-28 15:12:29 -0300

    erased VITE

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
-export const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL ?? "http://localhost:5173";
-export const BACKEND_URL = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080/api";
-export const AUTH0_USERNAME = import.meta.env.VITE_AUTH0_USERNAME ?? "";
-export const AUTH0_PASSWORD = import.meta.env.VITE_AUTH0_PASSWORD ?? "";
-export const AUTH0_AUDIENCE = import.meta.env.VITE_AUTH0_AUDIENCE ?? "";
-export const AUTH0_SCOPE = import.meta.env.VITE_AUTH0_SCOPE ?? "openid profile email";
+export const FRONTEND_URL = import.meta.env.FRONTEND_URL ?? "http://localhost:5173";
+export const BACKEND_URL = import.meta.env.API_BASE_URL ?? "http://localhost:8080/api";
+export const AUTH0_USERNAME = import.meta.env.AUTH0_USERNAME ?? "";
+export const AUTH0_PASSWORD = import.meta.env.AUTH0_PASSWORD ?? "";
+export const AUTH0_AUDIENCE = import.meta.env.AUTH0_AUDIENCE ?? "";
+export const AUTH0_SCOPE = import.meta.env.AUTH0_SCOPE ?? "openid profile email";
