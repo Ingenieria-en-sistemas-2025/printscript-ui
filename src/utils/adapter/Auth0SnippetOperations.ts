35d80a8e4ea6c821812437737334f7dff0ff3ff0
pilarsilveyra <psilveyra@mail.austral.edu.ar>
2025-11-06 15:34:28 -0300

    .

--- src/utils/adapter/Auth0SnippetOperations.ts
+++ src/utils/adapter/Auth0SnippetOperations.ts
@@ -6,7 +6,7 @@ import { TestCaseResult } from "../queries.tsx";
 import { FileType } from "../../types/FileType.ts";
 import { Rule } from "../../types/Rule.ts";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "/";
 
 export class Auth0SnippetOperations implements SnippetOperations {
     private getAccessTokenSilently: () => Promise<string>;
